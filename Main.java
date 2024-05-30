1a99f359da6defd606958aa73dc4c36857dccd7b
bilalsayed2000 <bilalsayed197@gmail.com>
2024-05-30 13:13:47 +0530

    second commit

--- Main.java
+++ Main.java
@@ -2,5 +2,15 @@ public class Main{
 
     public static void main(String[] args) {
         System.out.println("Main Class of VS-Code-Repository");
+        System.out.println("Added One line");
     }
+
+
+ /*echo "# VS-Code-Repository" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/bilalsayed2000/VS-Code-Repository.git
+git push -u origin main */
 }
